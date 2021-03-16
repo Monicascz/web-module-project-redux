35c721d729e8a72a17d22825a7ce27c2a004ac8a
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2021-03-16 14:42:53 -0700

    beginnings of redux have been set-up

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+//STEP 3: here is where you make your reducer. 
+
+const initialState={
+    state: ''
+}
+
+export const reducer = (state=initialState, action)=>{
+    switch(action.type){
+        default: return state;
+    }
+}
